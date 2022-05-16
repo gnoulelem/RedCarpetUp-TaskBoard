3a72043639cd297dfb49551f7dae2ef7ba2d487a
Gnoulelem SIBABI <gnoulelem.sibabi@bennu.tech>
2022-05-16 02:47:22 +0000

    implemented logout

--- src/components/Header/header.component.tsx
+++ src/components/Header/header.component.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getAuth, signOut } from 'firebase/auth';
 
 import logo from '../../assets/logo.png';
 import './header.styles.scss';
@@ -6,16 +7,33 @@ import './header.styles.scss';
 type HeaderProps = {};
 
 const Header: React.FC<HeaderProps> = () => {
+  const [showLogout, setShowLogout] = useState(false);
+
+  const handleLogout = async (): Promise<void> => {
+    const auth = getAuth();
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <header className="header">
       <div className="header-logo-container">
         <img width={40} src={logo} alt="Official Logo" /> <span>TaskBoard</span>
       </div>
       <img
+        onClick={() => setShowLogout(!showLogout)}
         src="https://picsum.photos/id/77/1631/1102"
         alt="Profile"
         className="header-profile"
       />
+      {showLogout && (
+        <div onClick={handleLogout} className="header-logout">
+          Logout
+        </div>
+      )}
     </header>
   );
 };
